e81afbf5c8254ed2fe64e69a0695b24fc49c8c47
sowmyakth <sowmya.spacebug@gmail.com>
2016-06-24 15:39:13 -0700

    height width of boxes are interchanged

--- make_reg.py
+++ make_reg.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def make_reg():
     """Make region file to identify objects in ds9"""
-    seg = '01'
+    seg = '0a'
     filters = ['f606w', 'f814w' ]
     main_path='/nfs/slac/g/ki/ki19/deuce/AEGIS/testing/zero_pt/comb_det/'+ seg +'/'
     main_file = 'EGS_10134_'+ seg +'_acs_wfc_30mas_unrot_added_drz.fits'
@@ -20,20 +20,23 @@ def make_reg():
         f.write(header1+header2+header3)
         cond1 = (main_cat['IS_STAR'] == 0)
         cond2 = (main_cat['IN_MASK'] == 0) 
-        cond3  =  (main_cat['SNR'] >= 20)
+        cond3  =  (main_cat['SNR'] >= 10)
         q, = np.where(cond1 & cond2 & cond3)
         for i in q:
             x0 = main_cat['X_IMAGE'][int(i)]
             y0 = main_cat['Y_IMAGE'][int(i)]
             r = main_cat['FLUX_RADIUS'][int(i)]
-            t = main_cat['THETA_IMAGE'][int(i)]
+            t = (main_cat['THETA_IMAGE'][int(i)])*np.pi/180.
             e = main_cat['ELLIPTICITY'][int(i)]
             A = 2.5*(main_cat['A_IMAGE'][int(i)])*(main_cat['KRON_RADIUS'][int(i)])
             x_size = A*(np.absolute(np.sin(t))+(1-e)*np.absolute(np.cos(t)))
             y_size = A*(np.absolute(np.cos(t))+(1-e)*np.absolute(np.sin(t)))
-            str1 = 'box(' + str(x0) +','+ str(y0) +','+ str(x_size) +','+ str(y_size)+','+ str(0) + ')'
-            str2 = str1 + ' # text={'+ str(main_cat['NUMBER'][int(i)] +1) + '} font={"2"} \n' 
+
+
+            str1 = 'box(' + str(x0) +','+ str(y0) +','+ str(y_size) +','+ str(x_size)+','+ str(0) + ')'
+            str2 = str1 + ' # color=green text={'+ str(main_cat['NUMBER'][int(i)]) + '} font={"2"} \n' 
             f.write(str2)
+
             #str1 =  'text(' + str(x0+x_size/2 ) +','+ str(y0+y_size/2 ) +') # text={'+ str(main_cat['NUMBER'][int(i)] +1) + '} \n'
             #f.write(str1)
             if main_cat['IS_BRIGHT'][int(i)] == 1:
